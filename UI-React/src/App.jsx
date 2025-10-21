80162de7bf71dbf76901b9becad8e69fd2e1f082
Kushagraawasthi37@gmail.com <Kushagraawasthi37@gmailcom>
2025-10-21 23:25:36 +0530

    incomplete ui

---
+++ UI-React/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Components/Section1/Header";
+import Section1 from "./Components/Section1/Section1";
+import Section2 from "./Components/Section2/Section2";
+
+
+function App() {
+  return (
+    <>
+    <Section1/>
+    <Section2/>
+    </>
+  );
+}
+
+export default App;
---
+++ UI-React/src/Components/Section1/Section1.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+
+const Section1 = () => {
+  return (
+    <div>
+      <div className="h-screen w-full bg-blue-950">
+        <Header />
+      </div>
+    </div>
+  );
+};
+
+export default Section1;
